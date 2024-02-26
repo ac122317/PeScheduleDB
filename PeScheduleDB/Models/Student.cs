a49482358b9d88e0960acf3d4adda1f46d986389
ac122317 <ac122317@8CG8163KY4>
2024-02-27 10:10:10 +1300

    Add project files.

---
+++ PeScheduleDB/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace PeScheduleDB.Models
+{
+    public class Student
+    {
+        public int Student_ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Emergency_Contact { get; set; }
+        public int Class_ID { get; set; }
+
+    }
+}
